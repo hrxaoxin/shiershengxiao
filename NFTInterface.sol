2feb6cf322013a17382b307f111dd1bfefac25fd
hrxaoxin <850753395@qq.com>
2026-06-11 21:58:45 +0800

    [   "0x1234567890abcdef1234567890abcdef12345678",   "0x55d398326f99059fF775485246999027B3197955",   "0x1887e7cC8303E26aD4BD23486881333A7C121184",   "0xD2908D072983503Cd54f68D7FD992aD5ea60A054",   "0xA9CBAbF288e821C4b19D1DAd8750c3dF2e53b5d9",   "0xC233Fcde95E16dE7B852f6a8E5509306A6512c3A",   "0x6F7C43971810bBF189E36546155C7C8912728A6B",   "0x8e8bEB884f419E04b50eeedeca2Fd8e8522e125b",   "0x35B1b1441a29eB5EAF7527fa88f7Cd94CEE0eB14",   "0xB3fe1F4E2E94253468BDFC6690Dba4A9871e750c",   "0x5813AE83ddeD8cF4D2a070A4B695510CaE1b481b",   "0x5480Cd670DB16b4ec635B54B9FBbA2643c1Fa1F0",   "0x561fd60b26b860aE84298C646eA8797c76BdeC84",   "0xe7DE02a826C3894dA7DEA2A4e526b1128B013F78",   "0x3b4D0Ad0e53cc7588aF127d2456B9D688B2186d6",   "0x3b4D0Ad0e53cc7588aF127d2456B9D688B2186d6",   "0xb7952f622e9622da4d46bF2a80f6695a18D70EA3",   "0x0a6D0f7fEDeC99a716C63930372ecfA75948092a",   "0x9f843738dB74052179dfFBF88ff365f99E760db4",   "0x96A5A17d0C475e5802a0dE8Ac4bDE43a69a86EbA",   "0x741F560216721b5060CF0752caEd9ebE902743AC",   "0x79Cdc416a1c4672F23F21516BaaB5Dc27d495C40",   "0x79Cdc416a1c4672F23F21516BaaB5Dc27d495C40",   "0xD2908D072983503Cd54f68D7FD992aD5ea60A054",   "0xeD96F81BcE06ca980d8c296Ce3D41A741957397A",   "0x84068C56E0d2124508B3f06D9265fb7aDCB950c2",   "0x9f5DFfDDE0364A8C6f29A494B818D681716180Ac",   "0x30Dc6b0eE830DbEBb2726E17d9aA0c364b3F846C",   "0xe7DE02a826C3894dA7DEA2A4e526b1128B013F78",   "0xf55A2a08De6AfF4B65AA5174c6C0305E2ba2B716",   "0xCB02ec8A0b5F73cea1b29375202443b2eB80A91D",   "0x10ED43C718714eb63d5aA57B78B54704E256024E",   "0xF3eeC923f3371F2b93658ad9430F1046B77B706c" ]
    [
      "0x1234567890abcdef1234567890abcdef12345678",
      "0x55d398326f99059fF775485246999027B3197955",
      "0x1887e7cC8303E26aD4BD23486881333A7C121184",
      "0xD2908D072983503Cd54f68D7FD992aD5ea60A054",
      "0xA9CBAbF288e821C4b19D1DAd8750c3dF2e53b5d9",
      "0xC233Fcde95E16dE7B852f6a8E5509306A6512c3A",
      "0x6F7C43971810bBF189E36546155C7C8912728A6B",
      "0x8e8bEB884f419E04b50eeedeca2Fd8e8522e125b",
      "0x35B1b1441a29eB5EAF7527fa88f7Cd94CEE0eB14",
      "0xB3fe1F4E2E94253468BDFC6690Dba4A9871e750c",
      "0x5813AE83ddeD8cF4D2a070A4B695510CaE1b481b",
      "0x5480Cd670DB16b4ec635B54B9FBbA2643c1Fa1F0",
      "0x561fd60b26b860aE84298C646eA8797c76BdeC84",
      "0xe7DE02a826C3894dA7DEA2A4e526b1128B013F78",
      "0x3b4D0Ad0e53cc7588aF127d2456B9D688B2186d6",
      "0x3b4D0Ad0e53cc7588aF127d2456B9D688B2186d6",
      "0xb7952f622e9622da4d46bF2a80f6695a18D70EA3",
      "0x0a6D0f7fEDeC99a716C63930372ecfA75948092a",
      "0x9f843738dB74052179dfFBF88ff365f99E760db4",
      "0x96A5A17d0C475e5802a0dE8Ac4bDE43a69a86EbA",
      "0x741F560216721b5060CF0752caEd9ebE902743AC",
      "0x79Cdc416a1c4672F23F21516BaaB5Dc27d495C40",
      "0x79Cdc416a1c4672F23F21516BaaB5Dc27d495C40",
      "0xD2908D072983503Cd54f68D7FD992aD5ea60A054",
      "0xeD96F81BcE06ca980d8c296Ce3D41A741957397A",
      "0x84068C56E0d2124508B3f06D9265fb7aDCB950c2",
      "0x9f5DFfDDE0364A8C6f29A494B818D681716180Ac",
      "0x30Dc6b0eE830DbEBb2726E17d9aA0c364b3F846C",
      "0xe7DE02a826C3894dA7DEA2A4e526b1128B013F78",
      "0xf55A2a08De6AfF4B65AA5174c6C0305E2ba2B716",
      "0xCB02ec8A0b5F73cea1b29375202443b2eB80A91D",
      "0x10ED43C718714eb63d5aA57B78B54704E256024E",
      "0xF3eeC923f3371F2b93658ad9430F1046B77B706c"
    ]

--- NFTInterface.sol
+++ NFTInterface.sol
@@ -76,6 +76,7 @@ interface INFTData {
  * @dev NFT铸造接口，提供铸造和查询功能
  */
 interface INFTMint {
+    function totalSupply() external view returns (uint256);
     function mint(address to, uint256 zodiacType) external returns (uint256);
     function mintNormal(address to) external returns (uint256);
     function mintRare(address to) external returns (uint256);
